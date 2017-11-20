9cf76746f0c2080f481b6fea88fe0ddc0374b83c
Tyler Langlois <tjl@byu.net>
2017-11-19 22:08:45 -0700

    fix testing for 5.x changes

--- spec/filters/sentimentalizer_spec.rb
+++ spec/filters/sentimentalizer_spec.rb
@@ -1,75 +1,78 @@
 require 'spec_helper'
-require "logstash/filters/sentimentalizer"
+require 'logstash/filters/sentimentalizer'
 
 describe LogStash::Filters::Sentimentalizer do
   describe 'default sentiment configuration' do
-    let(:config) do <<-CONFIG
-      filter {
-        sentimentalizer { }
-      }
-    CONFIG
+    let(:config) do
+      <<~CONFIG
+        filter {
+          sentimentalizer { }
+        }
+      CONFIG
     end
 
     sample 'Horrible' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['polarity']).to eq('negative')
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be < 0.5
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['polarity']).to eq('negative')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be < 0.5
     end
 
     sample 'Fantastic' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['polarity']).to eq('positive')
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be > 0.5
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['polarity']).to eq('positive')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be > 0.5
     end
   end
 
   describe 'twitter hashtag scoring without scrubbing' do
-    let(:config) do <<-CONFIG
-      filter {
-        sentimentalizer {
-            scrub => false
+    let(:config) do
+      <<~CONFIG
+        filter {
+          sentimentalizer {
+              scrub => false
+          }
         }
-      }
-    CONFIG
+      CONFIG
     end
 
     sample 'What a #horrible idea' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be_within(0.1).of(0.5)
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be_within(0.1).of(0.5)
     end
 
     sample 'What a #fantastic idea' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be_within(0.1).of(0.5)
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be_within(0.1).of(0.5)
     end
   end
 
   describe 'twitter hashtag scoring with scrubbing' do
-    let(:config) do <<-CONFIG
-      filter {
-        sentimentalizer {
-            scrub => true
+    let(:config) do
+      <<~CONFIG
+        filter {
+          sentimentalizer {
+              scrub => true
+          }
         }
-      }
-    CONFIG
+      CONFIG
     end
 
     sample 'What a #horrible idea' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['polarity']).to eq('negative')
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be < 0.1
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['polarity']).to eq('negative')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be < 0.1
     end
 
     sample 'What a #fantastic idea' do
-      expect(subject).to include("message")
-      expect(subject['sentiment']['polarity']).to eq('positive')
-      expect(subject['sentiment']['probability']).to be_a(Float)
-      expect(subject['sentiment']['probability']).to be > 0.9
+      expect(subject).to include('message')
+      expect(subject.get('sentiment')['polarity']).to eq('positive')
+      expect(subject.get('sentiment')['probability']).to be_a(Float)
+      expect(subject.get('sentiment')['probability']).to be > 0.9
     end
   end
 end
